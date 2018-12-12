aab0221984f174bc51420a352192e7c3fda42846
mokimo <bannedjobs@gmail.com>
2018-12-12 15:58:54 +0100

    fix(cleanUp): move documentTypes to config

--- plugins/gatsby-source-livingdocs/gatsby-node.js
+++ plugins/gatsby-source-livingdocs/gatsby-node.js
@@ -2,37 +2,9 @@ const liSDK = require('@livingdocs/node-sdk')
 const crypto = require('crypto')
 const resolveIncludes = require('./includes')
 const includesConfig = require('./includes/config')
+const {documentTypes, defaultDocumentType} = require('./includes/config/documentTypes')
 const renderLayout = require('./includes/render')
 const slugify = require('./slugify')
-
-const documentTypes = {
-  page: {
-    layoutComponents: {
-      layout: 'page-layout',
-      header: 'page-layout-header',
-      headerItem: 'page-layout-header-item',
-      footer: 'page-layout-footer'
-    }
-  },
-  article: {
-    layoutComponents: {
-      layout: 'article-layout',
-      header: 'article-layout-header',
-      headerItem: 'article-layout-header-item',
-      footer: 'article-layout-footer'
-    }
-  }
-}
-const defaultDocumentType = {
-  defaultDocumentType: {
-    layoutComponents: {
-      layout: 'default-layout',
-      header: 'default-layout-header',
-      headerItem: 'default-layout-header-item',
-      footer: 'default-layout-footer'
-    }
-  }
-}
 
 exports.sourceNodes = async ({actions}, configOptions) => {
   const {createNode} = actions
