2a4c90544fa4790beeef6041fd7718d7bf0fa146
mokimo <bannedjobs@gmail.com>
2018-12-12 14:58:20 +0100

    fix(blogPost): use config file, use custom eventhandling for <a/>-tag

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,61 +1,77 @@
 import React from 'react'
-import {graphql} from 'gatsby'
+import {graphql, navigate} from 'gatsby'
 import Layout from '../components/layout'
 import SocialMediaShareButtons from '../components/socialMediaButtons'
 import {Twitter, Facebook, Basic} from '../components/SEO'
-import {returnAuthorCard} from '../components/helpers'
+import {metadata} from '../../config'
 
 // The pages created in gatsby-node.js will use this component("template" by gatsby convention)
 // The data is recieved by the graphQL query from the bottom of this component
-const BlogPost = props => {
-  const authors =
-    props.data.publications.publication.metadata.authors &&
-    props.data.publications.publication.metadata.authors.references
+class BlogPost extends React.Component {
+  // add event listeners for all <a> tags (see handleClick, for the "why")
+  componentDidMount () {
+    for (const x of document.body.getElementsByTagName('a')) {
+      x.addEventListener('click', this.handleClick)
+    }
+  }
+  // remove event listerns for all <a> tags
+  componentWillUnmount () {
+    for (const x of document.body.getElementsByTagName('a')) {
+      x.removeEventListener('click', this.handleClick)
+    }
+  }
 
-  // SEO - description (Page, twitter, facebook)
-  const description = props.data.publications.publication.metadata.description
+  // Livingdocs will send a bunch of <a/> tags
+  // We don't want to reload the page, as we lose our cache / performance
+  // that's why we swap out the <a/> behaviour with the gatsby "navigate"
 
-  // SEO - title (Page, twitter, facebook)
-  const title = props.data.publications.publication.metadata.title
+  handleClick = event => {
+    // @TODO This is a hacky approach.
+    // Customize livingdocs instead to use <link> tags instead.
+    // This is not possible for service customers at the moment.
+    // Works for enterprise customers, please contact us if you need help regarding that
+    event.preventDefault()
+    const path = new URL(event.currentTarget.href).pathname
+    navigate(path)
+  }
+  render () {
+    // SEO - description (Page, twitter, facebook)
+    const description = this.props.data.publications.publication.metadata.description
 
-  // HTML - rendering the html-body
-  const html = props.data.publications.extra.html
+    // SEO - title (Page, twitter, facebook)
+    const title = this.props.data.publications.publication.metadata.title
 
-  // SEO - url (Page, twitter, facebook)
-  const url = props.data.site.siteMetadata.siteUrl
+    // HTML - rendering the html-body
+    const html = this.props.data.publications.extra.html
 
-  return (
-    <Layout>
-      {/* SEO start, information for the html <head></head> */}
-      <Basic
-        scripts={
-          // embed twitter script in the <head></head> if it exists
-          props.data.publications.publication.metadata.dependencies.js &&
-          props.data.publications.publication.metadata.dependencies.js[0].code
-        }
-        title={title}
-        description={description}
-      />
-      <Twitter title={title} description={description} url={url} />
-      <Facebook title={title} description={description} url={url} />
-      {/* SEO start, information for the html <head></head> */}
-      <div dangerouslySetInnerHTML={{__html: html}} />
-      {// this is just a fix because livingdocs does not provide author details
-        authors && authors.map(author => returnAuthorCard(author.id))}
-      <SocialMediaShareButtons title={title} description={description} />
-    </Layout>
-  )
+    // SEO - url (Page, twitter, facebook)
+    const url = metadata.url
+    return (
+      <Layout>
+        {/* SEO start, information for the html <head></head> */}
+        <Basic
+          scripts={
+            // embed twitter script in the <head></head> if it exists
+            this.props.data.publications.publication.metadata.dependencies.js &&
+            this.props.data.publications.publication.metadata.dependencies.js[0].code
+          }
+          title={title}
+          description={description}
+        />
+        <Twitter title={title} description={description} url={url} />
+        <Facebook title={title} description={description} url={url} />
+        {/* SEO start, information for the html <head></head> */}
+        <div dangerouslySetInnerHTML={{__html: html}} />
+        <SocialMediaShareButtons title={title} description={description} />
+      </Layout>
+    )
+  }
 }
 
 // in gatsby-node.js we gave a context "slug"
 // we can use that to filter the matching publication and slug now
 export const query = graphql`
   query($slug: String!) {
-    site {
-      siteMetadata {
-        siteUrl
-      }
-    }
     publications(extra: {slug: {eq: $slug}}) {
       publication {
         metadata {
