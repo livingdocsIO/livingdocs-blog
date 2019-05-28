d0e12b828f14d86a164f9fc7a3ab9811493ff2c2
mokimo <bannedjobs@gmail.com>
2019-05-28 23:03:22 +0200

    fix(plugin): improve plugin error logging

--- plugins/gatsby-source-livingdocs/gatsby-node.js
+++ plugins/gatsby-source-livingdocs/gatsby-node.js
@@ -1,3 +1,5 @@
+/* eslint-disable max-len */
+/* eslint-disable no-console */
 const liSDK = require('@livingdocs/node-sdk')
 const crypto = require('crypto')
 const resolveIncludes = require('./includes')
@@ -6,6 +8,10 @@ const renderLayout = require('./includes/render')
 const slugify = require('./slugify')
 
 exports.sourceNodes = ({actions}, configOptions) => {
+  if (!configOptions.design) return console.warn('config.options.design missing')
+  if (!configOptions.design.name) return console.warn("config.options.design.name missing example: 'living-times'")
+  if (!configOptions.design.version) return console.warn("config.options.design.version missing example: '0.0.1'")
+
   const {createNode} = actions
 
   // Gatsby adds a configOption that's not needed for this plugin, delete it
@@ -20,10 +26,7 @@ exports.sourceNodes = ({actions}, configOptions) => {
   const limit = configOptions.limit ? configOptions.limit : 10
 
   // get all publications (articles, authors, etc.)
-  const getAllPublications = () => {
-    const publication = liClient.getPublications({limit}).then(publications => publications)
-    return publication
-  }
+  const getAllPublications = async () => liClient.getPublications({limit})
 
   const getPublication = async (publication, design) => {
     const livingdoc = liSDK.document.create({
@@ -46,7 +49,10 @@ exports.sourceNodes = ({actions}, configOptions) => {
 
   // Create your node object
   const processPublication = async (publication, design) => {
+    console.log('UEUHWGUH393skdg')
+    console.log(publication)
     const html = await getPublication(publication, design)
+    console.log(html)
     const nodeData = {
       id: `${publication.systemdata.documentId}`,
       parent: `__SOURCE__`,
@@ -65,7 +71,18 @@ exports.sourceNodes = ({actions}, configOptions) => {
   }
   async function createNodes () {
     const allPublications = await getAllPublications()
-    const design = await liClient.getDesign({name: 'living-times', version: '0.0.14'})
+    if (!allPublications.length) {
+      console.warn(`
+      ALERT! Livingdocs-gatsby-plugin has not found any publications.
+      Are you sure you've added published documents to livingdocs?
+      `)
+    }
+
+    const design = await liClient.getDesign({
+      name: configOptions.design.name,
+      version: configOptions.design.version
+    })
+    if (!design) console.warn('ALERT! livingdocs-gatsby-plugin: design could not be loaded')
     for (const publication of allPublications) {
       const nodeData = await processPublication(publication, design)
       createNode(nodeData)
