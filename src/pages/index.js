ffef38be53122ce51dff9563805c37ef9deff473
mokimo <bannedjobs@gmail.com>
2019-05-29 15:36:31 +0200

    chore(pages): remove demo filter buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,53 +5,16 @@ import Layout from '../components/layout'
 
 // The homepage. This could also be done in Livingdocs
 class Homepage extends React.Component {
-  /* Filter article logic start */
-
-  state = {
-    filter: 'none'
-  }
-
-  setFilter = filter => {
-    this.setState({filter})
-  }
-
-  filterElements = (categorie, content) =>
-    this.state.filter === 'none' ? content : this.state.filter === categorie && content
-
-  createFilterButtons = () => (
-    <header className="page-head" role="banner">
-      <nav className="main-nav" role="navigation">
-        {['none', 'technology', 'immigration', 'politics', 'environment'].map(filterType => (
-          <div className="main-nav__item" key={filterType}>
-            <a
-              href={`#${filterType}`}
-              onClick={() => this.setFilter(filterType)}
-              style={{
-                backgroundColor: this.state.filter === filterType && '#eee'
-              }}
-            >
-              {filterType}
-            </a>
-          </div>
-        ))}
-      </nav>
-    </header>
-  )
-
-  /* Filter article logic end */
 
   render () {
     return (
       <Layout>
-        {this.createFilterButtons()}
         {this.props.data.allPublications.edges.map(data =>
-          this.filterElements(
-            <BlogCard
-              {...data.node.publication.metadata}
-              slug={data.node.extra.slug}
-              key={data.node.publication.systemdata.documentId}
-            />
-          )
+          <BlogCard
+            {...data.node.publication.metadata}
+            slug={data.node.extra.slug}
+            key={data.node.publication.systemdata.documentId}
+          />
         )}
       </Layout>
     )
