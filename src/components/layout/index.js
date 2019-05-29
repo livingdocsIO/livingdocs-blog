930d5266234fb4345e18876822832e1e3426a823
mokimo <bannedjobs@gmail.com>
2019-05-29 17:30:15 +0200

    feat: guard links & move some layout to component

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -5,37 +5,64 @@ import Footer from '../footer'
 import favicon from '../resources/favicon.png'
 import '../../resources/stylesheets/living-stories.scss'
 import {metadata} from '../../../config.js'
+import {navigate} from 'gatsby'
 
 // Set a basic set of SEO data
 // blog-posts and author-pages will over overwrite some of them
-const Layout = ({children}) => (
-  <div>
-    <Helmet defaultTitle={metadata.defaultTitle} titleTemplate={metadata.titleTemplate}>
-      <meta name="author" content={metadata.author} />
-      <meta name="description" content={metadata.description} />
-      <meta name="keywords" content={metadata.keywords} />
-      <meta name="viewport" content="width=device-width" />
-      <html lang="en" />
-      <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta httpEquiv="Content-Language" content="en" />
-      <meta property={metadata.facebookId} content="id" />
-      <meta property={metadata.twitterId} content="id" />
+class Layout extends React.Component {
 
-      <link title="timeline-styles" rel="shortcut icon" type="image/png" href={favicon} />
-      <noscript>Please enable Javascript</noscript>
-    </Helmet>
-    <Header />
-    <main className="wrapper wrapper--default" role="main">
-      <div className="container container--breath">
-        <div className="container-grid container-grid--whole">
-          <div className="container-grid__item">
-            {children}
-          </div>
-        </div>
+  componentDidMount () {
+    // @TODO This is a hacky approach.
+    // Livingdocs will send a bunch of <a/> tags, we will addEventListeners for those.
+    // We don't want to reload the page, because we lose a big chunk of performance.
+    // that's why we swap out the <a/> behaviour with the gatsby "navigate", for internal links.
+    for (const anchorTag of document.body.getElementsByTagName('a')) {
+      this.internalLink(anchorTag) && anchorTag.addEventListener('click', this.handleClick)
+    }
+  }
+
+  // remove event listerns for all <a> tags
+  componentWillUnmount () {
+    for (const anchorTag of document.body.getElementsByTagName('a')) {
+      this.internalLink(anchorTag) && anchorTag.removeEventListener('click', this.handleClick)
+    }
+  }
+
+  internalLink = link => link.host === window.location.host
+
+  handleClick = event => {
+    event.preventDefault()
+    const path = new URL(event.currentTarget.href).pathname
+    navigate(path)
+  }
+
+  render () {
+    // eslint-disable-next-line max-len
+    const {defaultTitle, titleTemplate, author, description, keywords, facebookId, twitterId} = metadata
+    return (
+      <div>
+        <Helmet defaultTitle={defaultTitle} titleTemplate={titleTemplate}>
+          <meta name="author" content={author} />
+          <meta name="description" content={description} />
+          <meta name="keywords" content={keywords} />
+          <meta name="viewport" content="width=device-width" />
+          <html lang="en" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta httpEquiv="Content-Language" content="en" />
+          <meta property={facebookId} content="id" />
+          <meta property={twitterId} content="id" />
+
+          <link title="timeline-styles" rel="shortcut icon" type="image/png" href={favicon} />
+          <noscript>Please enable Javascript</noscript>
+        </Helmet>
+        <Header />
+        <main className="wrapper wrapper--default" role="main">
+          {this.props.children}
+        </main>
+        <Footer />
       </div>
-    </main>
-    <Footer />
-  </div>
-)
+    )
+  }
+}
 export default Layout
