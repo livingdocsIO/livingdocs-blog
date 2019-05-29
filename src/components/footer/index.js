4e4609c92629b32109103215ddcf76cbafa36be9
mokimo <bannedjobs@gmail.com>
2019-05-29 15:35:34 +0200

    feat(footer): revamp to living-stories design

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,21 +2,26 @@ import React from 'react'
 
 const footer = () => (
   // eslint-disable-next-line
-  <footer className="page-foot" role="footer">
-    <nav className="page-foot__wrapper">
-      <ul className="footer-links footer-links--left-aligned">
-        <li>© 2018 Livingdocs Times</li>
-        <li>
-          <a href="mailto:contact@livingdocs.io">Contact us</a>
-        </li>
-        <li>
-          <a href="https://www.livingdocs.io/product-5919">Work With us</a>
-        </li>
-        <li>
-          <a href="https://github.com/livingdocsIO/magazine-example">Github</a>
-        </li>
-      </ul>
-    </nav>
+  <footer class="page-foot" role="footer">
+    <div className="wrapper wrapper--default">
+      <nav className="page-foot-nav">
+        <ul className="footer-links footer-links--left-aligned">
+          <li>© 2018 Living Stories</li>
+          <li>
+            <a href="mailto:contact@livingdocs.io">Contact us</a>
+          </li>
+          <li>
+            <a href="https://www.livingdocs.io">Work With us</a>
+          </li>
+          <li>
+            <a href="https://github.com/livingdocsIO/blog-example">Github</a>
+          </li>
+        </ul>
+        <ul className="footer-links footer-links--right-aligned">
+          <li>This is an open-source demo blog built with Livingdocs.</li>
+        </ul>
+      </nav>
+    </div>
   </footer>
 )
 
