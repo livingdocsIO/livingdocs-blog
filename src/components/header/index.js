805dbf6c09db44b223fbc5660e34f44858866177
mokimo <bannedjobs@gmail.com>
2019-05-29 17:33:39 +0200

    chore(templates): remove <a/> tag hack

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import logo from '../../resources/assets/images/li-stories-logo.svg'
 
+
+// TODO <a className="logo" href="/"> should be a link - plz check
 const Header = () => (
   <header className="page-head" role="banner">
     <div>
@@ -11,10 +13,10 @@ const Header = () => (
           </a>
           <nav className="meta-nav">
             <div className="meta-nav__item">
-              <a href="https://github.com/livingdocsIO/blog-example" target="_blank">Source Code</a>
+              <a href="https://github.com/livingdocsIO/blog-example" target="_blank" rel="noopener noreferrer">Source Code</a>
             </div>
             <div className="meta-nav__item meta-nav__item--highlight">
-              <a href="https://edit.livingdocs.io/signup">Sign Up</a>
+              <a href="https://edit.livingdocs.io/signup" target="_blank" rel="noopener noreferrer">Sign Up</a>
             </div>
           </nav>
         </div>
--- src/templates/pages.js
+++ src/templates/pages.js
@@ -1,53 +1,23 @@
 import React from 'react'
-import {graphql, navigate} from 'gatsby'
+import {graphql} from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/SEO'
 import {metadata} from '../../config'
 
-// The pages created in gatsby-node.js will use this component("template" by gatsby convention)
-// The data is recieved by the graphQL query from the bottom of this component
-class BlogPost extends React.Component {
-  // add event listeners for all <a> tags (see handleClick, for the "why")
-  componentDidMount () {
-    for (const x of document.body.getElementsByTagName('a')) {
-      x.addEventListener('click', this.handleClick)
-    }
-  }
-  // remove event listerns for all <a> tags
-  componentWillUnmount () {
-    for (const x of document.body.getElementsByTagName('a')) {
-      x.removeEventListener('click', this.handleClick)
-    }
-  }
-
-  // Livingdocs will send a bunch of <a/> tags
-  // We don't want to reload the page, as we lose our cache / performance
-  // that's why we swap out the <a/> behaviour with the gatsby "navigate"
-
-  handleClick = event => {
-    // @TODO This is a hacky approach.
-    // Customize livingdocs instead to use <link> tags instead.
-    // This is not possible for service customers at the moment.
-    // Works for enterprise customers, please contact us if you need help regarding that
-    event.preventDefault()
-    const path = new URL(event.currentTarget.href).pathname
-    navigate(path)
-  }
-  render () {
+const page = props => {
   // SEO - description | title | url
-    const description = this.props.data.publications.publication.metadata.description
-    const title = this.props.data.publications.publication.metadata.title
-    const url = metadata.url
+  const description = props.data.publications.publication.metadata.description
+  const title = props.data.publications.publication.metadata.title
+  const url = metadata.url
 
-    // HTML - rendering the html-body
-    const html = this.props.data.publications.extra.html
-    return (
-      <Layout>
-        <SEO title={title} description={description} url={url}/>
-        <div dangerouslySetInnerHTML={{__html: html}} />
-      </Layout>
-    )
-  }
+  // HTML - rendering the html-body
+  const html = props.data.publications.extra.html
+  return (
+    <Layout>
+      <SEO title={title} description={description} url={url}/>
+      <div dangerouslySetInnerHTML={{__html: html}} />
+    </Layout>
+  )
 }
 
 export const query = graphql`
@@ -66,4 +36,4 @@ export const query = graphql`
     }
   }
 `
-export default BlogPost
+export default page
