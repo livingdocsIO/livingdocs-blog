69e3332e62aa9f93e614d479bc2cde82c508f216
mokimo <bannedjobs@gmail.com>
2019-05-29 17:33:39 +0200

    chore(templates): remove <a/> tag hack

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,58 +1,26 @@
 import React from 'react'
-import {graphql, navigate} from 'gatsby'
+import {graphql} from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/SEO'
 import {metadata} from '../../config'
 
-// The pages created in gatsby-node.js will use this component("template" by gatsby convention)
-// The data is recieved by the graphQL query from the bottom of this component
-class BlogPost extends React.Component {
-  // add event listeners for all <a> tags (see handleClick, for the "why")
-  componentDidMount () {
-    for (const x of document.body.getElementsByTagName('a')) {
-      x.addEventListener('click', this.handleClick)
-    }
-  }
-  // remove event listerns for all <a> tags
-  componentWillUnmount () {
-    for (const x of document.body.getElementsByTagName('a')) {
-      x.removeEventListener('click', this.handleClick)
-    }
-  }
+const blogPost = props => {
+  // SEO - description | title | url
+  const description = props.data.publications.publication.metadata.description
+  const title = props.data.publications.publication.metadata.title
+  const url = metadata.url
 
-  // Livingdocs will send a bunch of <a/> tags
-  // We don't want to reload the page, as we lose our cache / performance
-  // that's why we swap out the <a/> behaviour with the gatsby "navigate"
+  // HTML - rendering the html-body
+  const html = props.data.publications.extra.html
 
-  handleClick = event => {
-    // @TODO This is a hacky approach.
-    // Customize livingdocs instead to use <link> tags instead.
-    // This is not possible for service customers at the moment.
-    // Works for enterprise customers, please contact us if you need help regarding that
-    event.preventDefault()
-    const path = new URL(event.currentTarget.href).pathname
-    navigate(path)
-  }
-  render () {
-    // SEO - description | title | url
-    const description = this.props.data.publications.publication.metadata.description
-    const title = this.props.data.publications.publication.metadata.title
-    const url = metadata.url
-
-    // HTML - rendering the html-body
-    const html = this.props.data.publications.extra.html
-
-    return (
-      <Layout>
-        <SEO title={title} description={description} url={url}/>
-        <div dangerouslySetInnerHTML={{__html: html}} />
-      </Layout>
-    )
-  }
+  return (
+    <Layout>
+      <SEO title={title} description={description} url={url}/>
+      <div dangerouslySetInnerHTML={{__html: html}} />
+    </Layout>
+  )
 }
 
-// in gatsby-node.js we gave a context "slug"
-// we can use that to filter the matching publication and slug now
 export const query = graphql`
   query($slug: String!) {
     publications(extra: {slug: {eq: $slug}}) {
@@ -69,4 +37,4 @@ export const query = graphql`
     }
   }
 `
-export default BlogPost
+export default blogPost
