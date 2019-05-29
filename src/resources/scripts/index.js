760513c4514ab637176e4ea6ccb94584b1e7b13f
mokimo <bannedjobs@gmail.com>
2019-05-29 15:07:33 +0200

    feat(scss): add sass support

---
+++ src/resources/scripts/index.js
@@ -0,0 +1,9 @@
+// css deps
+require('photoswipe/dist/photoswipe.css')
+require('photoswipe/dist/default-skin/default-skin.css')
+
+// living stories design
+require('../stylesheets/living-stories.scss')
+
+// initialise image gallery
+require('./init_gallery_teasers')(window, document)
