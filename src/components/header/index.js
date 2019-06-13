5a659eef2a8b3509f861c4d9b9316274d19bb1f2
mokimo <bannedjobs@gmail.com>
2019-06-13 16:22:43 +0200

    feat(header): use livingdocs logo and remove todo

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,24 +1,24 @@
 import React from 'react'
-import logo from '../../resources/assets/images/li-stories-logo.svg'
+import logo from '../../resources/assets/images/ld-logo.svg'
 
-
-// TODO <a className="logo" href="/"> should be a link - plz check
 const Header = () => (
   <header className="page-head" role="banner">
     <div>
       <div className="wrapper wrapper--default">
         <div className="meta-bar">
-          <a className="logo" href="/">
-            <img src={logo} alt="Living Stories" />
-          </a>
-          <nav className="meta-nav">
-            <div className="meta-nav__item">
-              <a href="https://github.com/livingdocsIO/blog-example" target="_blank" rel="noopener noreferrer">Source Code</a>
-            </div>
-            <div className="meta-nav__item meta-nav__item--highlight">
-              <a href="https://edit.livingdocs.io/signup" target="_blank" rel="noopener noreferrer">Sign Up</a>
-            </div>
-          </nav>
+          <div className="meta-bar__unit">
+            <a className="meta-bar__logo logo" href="/">
+              <img src={logo} alt="Living Stories" />
+            </a>
+            <b className="meta-bar__title">Livingdocs Blog</b>
+          </div>
+          <div className="meta-bar__unit">
+            <nav className="meta-nav">
+              <div className="meta-nav__item meta-nav__item--highlight">
+                <a href="https://edit.livingdocs.io/signup" target="_blank" rel="noopener noreferrer">Sign Up</a>
+              </div>
+            </nav>
+          </div>
         </div>
       </div>
       <div className="main-nav-container">
