0401ac40a96c31271f16c60be8a5e1fb251f7b37
mokimo <bannedjobs@gmail.com>
2019-07-02 17:09:12 +0200

    fix(config): set correct metadata for crawlers

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
 // The standard metadata will be overwritten dynamically (blogPost, authorData)
 export const metadata = {
-  defaultTitle: 'Livingblogs',
-  titleTemplate: '%s - Livingblogs',
+  defaultTitle: 'Livingdocs Blog',
+  titleTemplate: '%s - Livingdocs Blog',
   author: 'Livingdocs AG, Zürich, Switzerland',
-  keywords: 'Livingdocs blog example livingblogs',
-  description: `This is the Livingdocs-blog.
-  We publish content all around Javascript,
-  Frameworks and Livingdocs itself.`,
+  keywords: 'Livingdocs blog example',
+  description:
+  `
+    Here we blog and publish content all around technology,
+    partnerships and obviously Livingdocs itself.
+  `,
   facebookId: 'fb:page_id',
   twitterId: 'twitter:account_id',
   url: 'https://blog.livingdocs.io'
