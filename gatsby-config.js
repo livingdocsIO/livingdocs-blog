615bc9114aea69e4adae115e67a47fdd736caacc
mokimo <bannedjobs@gmail.com>
2019-09-04 18:49:39 +0200

    feat(analytics): add google analytics script

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,6 +52,17 @@ module.exports = {
         display: 'minimal-ui',
         icon: 'src/resources/assets/images/favicon.png'
       }
+    },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: 'UA-47320912-2',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true
+      }
     }
     // 'gatsby-plugin-offline' caused some problems
   ]
