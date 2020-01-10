7f0abae7ab43a17228e7d10061ac474e23ec4df8
mokimo <bannedjobs@gmail.com>
2020-01-10 10:41:42 +0100

    chore: add name properties

--- src/components/SEO/openGraph.js
+++ src/components/SEO/openGraph.js
@@ -4,14 +4,11 @@ import Helmet from 'react-helmet'
 // Open Graph for various social media websites / example: Facebook, twitter cards
 const openGraph = props => (
   <Helmet>
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content={props.url} />
-    <meta property="og:title" content={props.title} />
-    <meta property="og:description" content={props.description} />
-    <meta
-      property="og:image"
-      content={props.imageUrl}
-    />
+    <meta name="type" property="og:type" content="website" />
+    <meta name="url" property="og:url" content={props.url} />
+    <meta name="title" property="og:title" content={props.title} />
+    <meta name="description" property="og:description" content={props.description} />
+    <meta name="image" property="og:image" content={props.imageUrl} />
   </Helmet>
 )
 
