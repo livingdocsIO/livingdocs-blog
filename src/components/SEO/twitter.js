e88ef5705975c66fa6c04185b220a3041382e640
mokimo <bannedjobs@gmail.com>
2020-05-18 11:16:55 +0200

    fix: bump dependencies

--- src/components/SEO/twitter.js
+++ src/components/SEO/twitter.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Helmet from 'react-helmet'
+import {Helmet} from 'react-helmet'
 
 // twitter:title, twitter:description, twitter:image, twitter:url
 // are derived from the OG data
